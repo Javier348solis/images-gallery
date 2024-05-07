d3072f6b23d1d1176d2267a99aef86df0db8811b
Javier348solis <javiersolisretana@gmail.com>
2024-05-07 09:37:31 -0600

    addition number2

--- Practice.js
+++ Practice.js
@@ -4,6 +4,7 @@ var input3 = document.getElementById("input3")
 var btn4 = document.getElementById("btn4")
 
 btn4.addEventListener("click", (e) => {
+    //datos localStorage
     if (input1.value === "javiersolisretana@gmail.com" && input2.value === "Javier12pxc56" && input3.value === "123456") {
        e.preventDefault()
         window.location.href="crear-cuenta.html"
@@ -11,3 +12,4 @@ btn4.addEventListener("click", (e) => {
         alert("Datos incorectos")
     }
 })
+//Hacer conversion de JAsonArray 
