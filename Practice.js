8d1e90d9bad39006418b18e07c6987f5fc8da590
Javier348solis <javiersolisretana@gmail.com>
2024-05-10 13:22:23 -0600

    New addition nmr4

--- Practice.js
+++ Practice.js
@@ -5,36 +5,17 @@ var btn4 = document.getElementById("btn4")
     
 btn4.addEventListener("click", (e) => {
 
-
-
-    const Users = JSON.parse(localStorage.getItem("listaUsuarios"))
-
-   console.log(Users[1].correo)
-
-   if (input1.value ==Users[1].correo) {
-    
-   
-   }else{
-    console.log('correo invalido')
-
-    const validacionUsuario = Users.find(user => user.correo === input1.value && user.contrasenia === input2.value)
+    const listaUsuarios = JSON.parse(localStorage.getItem("listaUsuarios"))
+    let usuarios = {
+      nombre: input1.value,
+      correo: input2.value,
+      contrasenia: input3.value,
+      }
+      
+      let usuarioEncontrado = listaUsuarios.find(u=>input1.value === nombre.value && input2.value===correo.value)
+      if (usuarioEncontrado) {
+        alert("INGRESA")  
+      }
  
-    if(validacionUsuario){
-     alert("datos correctos")
-    
-    }else{
-     alert("DATOS INCORRECTOS, METASE EN LA VARA")
-    }
- 
-     // if (input1.value === "javiersolisretana@gmail.com" && input2.value === "Javier12pxc56" && input3.value === "123456") {
-     //    e.preventDefault()
-     // } else {
-         
-     // }  
-   }
- 
- 
-})
-
-
-//users[index].correo   
+   } 
+)
--- SIGNUP.js
+++ SIGNUP.js
@@ -1,38 +1,30 @@
 const boton3 = document.getElementById("boton3")
-
 const nombre = document.getElementById("Nombre")
 const correo = document.getElementById("Correo")
 const contrasena = document.getElementById("Contrasena")
 const confirmar = document.getElementById("Confirmar")
-
 //recordar la info al array 
-let listaUsuarios=[]
+let listaUsuarios=JSON.parse(localStorage.getItem("listaUsuarios")) || [] // Usamos un arreglo para traer los datos del localStorage. Si no hay datos, el arreglo estará vacío
 
-boton3.addEventListener("click", (e)=>{
-    e.preventDefault()
-  
-  
-
-    let usuarios = {
-    nombre: nombre.value,
-    correo: correo.value,
-    contrasenia: contrasena.value,
-    confirmar:confirmar.value,
-    }
-
-
-
-    listaUsuarios.push(usuarios)
-
-
-    localStorage.setItem("listaUsuarios",JSON.stringify(listaUsuarios));
-
+    boton3.addEventListener("click", (e)=>{
+        e.preventDefault()
+        let usuarios = {
+        nombre: nombre.value,
+        correo: correo.value,
+        contrasenia: contrasena.value,
+        confirmar:confirmar.value,
+        } // Usamos un objeto, para guardar más de un dato, dentro del nombre de una misma variable
     
-   /* const isUsersRegistered = Users.find(user => user.correo === Correo.value)
-    if (isUsersRegistered) {
-        return alert("El usuario ya esta registrado")
+        if(listaUsuarios.length>0){ // Usamos el if, para validar si el arreglo tiene datos, si tiene datos comprobará si el usuario existe, si no tiene datos únicamnete creará el usuario.
+            for(let i = 0; i < listaUsuarios.length; i++) {
+            if (nombre.value == listaUsuarios[i].nombre) {
+            alert("EL USUARIO YA ESTÁ REGISTRADO")
+            return;
+            }
+        }
     }
-    Users.push(usuarios)
-    localStorage.setItem("users",JSON.stringify(Users))
-    */
-})
+    listaUsuarios.push(usuarios)
+    localStorage.setItem("listaUsuarios",JSON.stringify(listaUsuarios))
+    //Acá hacemos la creación del usuario, primero utilizamos el push, para insertar al array que tendrá todos los datos
+    // El localStorage se encarga de guardar sobre el nombre de llave listaUsuarios, el dato convertido de JavaScript, que sería el array a un formato JSON
+    })
